99dc966c4652f3a288ef7ddfb44578ff585aca59
khalidpat <99796067+khalidpat@users.noreply.github.com>
2022-02-16 14:33:59 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">@2021 All rights reserved, Beans & Dough. Developed By - Khalid Patrawala</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
+
+
---
+++ contact.php
@@ -0,0 +1,13 @@
+ <?php include('partials-front/menu.php'); ?>
+    <section class="food-search text-center">
+        <div class="container">
+            <h2>Beans And Dough "Contact Us At: +91999999999"  <class="text-white"></h2>
+
+        </div>
+    </section>
+
+
+
+
+
+<?php include('partials-front/footer.php'); ?>
